6ccd2fb3f96629306dbf5309b42ec97cf28ddd7e
Wira Chanra <wirachanra08@gmail.com>
2022-12-18 14:30:11 +0700

    Hapus angka 1 dan 2 dan 8 8

--- src/pages/aboutme/aboutme.tsx
+++ src/pages/aboutme/aboutme.tsx
@@ -16,15 +16,11 @@ export default function Home() {
    <div className="header">
     {/* ul>li*5>a{Link $$} contoh emet mempercepat buat ul dan li hasil dibawah */}
     <ul>
-     <li><a href="">link 01</a></li>
-     <li><a href="">link 02</a></li>
      <li><a href="">link 03</a></li>
      <li><a href="">link 04</a></li>
      <li><a href="">link 05</a></li>
      <li><a href="">link 06</a></li>
      <li><a href="">link 07</a></li>
-     <li><a href="">link 08</a></li>
-     <li><a href="">link 08</a></li>
      <li><a href="">link 08</a></li>
     </ul>
    </div>
