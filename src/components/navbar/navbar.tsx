1065ffa7582b6683da6676536484536d9ad6af49
Wira Chanra <wira.chanra@lixus.id>
2022-12-18 14:03:19 +0700

    commit pertama di testing 2 branch

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -14,6 +14,14 @@ export default function Home() {
     <li><a href="">link 04</a></li>
     <li><a href="">link 05</a></li>
    </ul>
+   <ul>
+    <li><a href="">navbar</a></li>
+    <li><a href="">link 02</a></li>
+    <li><a href="">link 03</a></li>
+    <li><a href="">link 04</a></li>
+    <li><a href="">link 05</a></li>
+   </ul>
+
   </div>
  )
 }
